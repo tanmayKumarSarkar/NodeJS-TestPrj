e75d19dcc6922d99790bf4c9843d96d9ad12358f
tanmaykumar1993 <tanmay.kr.sarkar@live.com>
2018-04-30 15:56:36 +0530

    update

--- AngTest2/server.js
+++ AngTest2/server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
 
 app.get('*', (req, res)=>{
-	//res.sendFile(path.join(__dirname, 'dist/index.html'));
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 var port = process.env.PORT || '3000';
